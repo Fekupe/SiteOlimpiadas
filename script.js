436ce0d53b3594763565d6a497efc4dcddc93544
Fekupe <fekupe@outlook.com>
2023-02-02 13:31:26 -0300

    teste

---
+++ script.js
@@ -0,0 +1,21 @@
+// let entrada = prompt("Digite o ano que você nasceu");
+// alert(2023 - entrada);
+// alert(typeof entrada);
+let body = document.querySelector("body");
+
+function toDarkSide() {
+  body.classList.add("dark");
+  body.classList.remove("light");
+}
+
+function toJediSide() {
+  body.classList.add("light");
+  body.classList.remove("dark");
+}
+
+function toJediSide() {
+  console.log("OK");
+
+  body.classList.add("light");
+  body.classList.remove("dark");
+}
