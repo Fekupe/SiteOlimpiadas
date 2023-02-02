4afd46bf024b4dab670c0e752aec48c8ade330f9
Fekupe <fekupe@outlook.com>
2023-02-02 15:22:20 -0300

    penultima atualização

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // let entrada = prompt("Digite o ano que você nasceu");
 // alert(2023 - entrada);
 // alert(typeof entrada);
+
+
 let body = document.querySelector("body");
+//let nav = document.querySelector("nav");
+
+
+//function toDarkSideNav() {
+  //nav.classList.add("dark-nav");
+  //nav.classList.remove("light-nav");
+//}
+
+//function toJediSideNav() {
+  //nav.classList.add("light-nav");
+  //nav.classList.remove("dark-nav");
+//}
 
 function toDarkSide() {
   body.classList.add("dark");
@@ -19,3 +33,11 @@ function toJediSide() {
   body.classList.add("light");
   body.classList.remove("dark");
 }
+
+//function toJediSideNav() {
+  //console.log("OK");
+  //nav.classList.add("light-nav");
+  //nav.classList.remove("dark-nav");
+//}
+
+// só um teste do toggle pra ver se funciona 
